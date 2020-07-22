d7ec8e3357be44f6c5d96f0298b348b54ec8c76d
Emmanuel Ogbonna <emmanuel@Emmanuels-MacBook-Air.local>
2020-07-22 18:24:01 +0100

    Updates

---
+++ precache-manifest.ce6ec31916e73a17582bef675b8d6956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37638ef6cf650d50bf7aa7d1e5eb1556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a90314f5666221ca7b08",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be02259aed7e5ef5cc5c",
+    "url": "/static/js/2.d04e9002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d04e9002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90314f5666221ca7b08",
+    "url": "/static/js/main.8c5e97c4.chunk.js"
+  },
+  {
+    "revision": "5e22ce6e59cc7fa8b44a",
+    "url": "/static/js/runtime-main.ceb8f294.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
